69fdfd449fbb78bc399fc19e23653d0b1ba066ac
poonks <cpoonkodi@gmail.com>
2021-10-17 20:12:25 +0300

    Added images and marquee

--- Source/js/main.js
+++ Source/js/main.js
@@ -4,3 +4,36 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+
+
+
+const root=document.documentElement;
+const marqueeElementsDisplayed=getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent=document.querySelector(".marquee-content");
+
+root.style.setProperty("--marquee-elements",marqueeContent.children.lengh);
+
+for (let i=0; i<marqueeElementsDisplayed;i++){
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
